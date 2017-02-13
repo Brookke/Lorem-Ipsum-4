2634f0afed36653d963c3d299d607dc92f5657ff
ab1892 <ab1892@york.ac.uk>
2017-02-13 15:19:56 +0000

    Removed mouse input variables and methods

--- core/src/me/lihq/game/people/controller/OtherInput.java
+++ core/src/me/lihq/game/people/controller/OtherInput.java
@@ -11,21 +11,6 @@ import com.badlogic.gdx.InputProcessor;
 public class OtherInput implements InputProcessor {
 	
 	/**
-	 * Private variable representing the mouse's X coordinate on the screen
-	 */
-	private static int mouseX = -1;
-	
-	/** 
-	 * Private variable representing the mouse's Y coordinate on the screen
-	 */
-	private static int mouseY = -1;
-	
-	/**
-	 * Private variable representing the mouse button value being pressed
-	 */
-	private static int mouseB = -1;
-	
-	/**
 	 * Private array storing the boolean state of keys. The index of the array is the key value from libgdx. True means key is pressed, False means key is not pressed
 	 */
 	private boolean[] keys = new boolean[256];
@@ -35,33 +20,6 @@ public class OtherInput implements InputProcessor {
 	 * E.g. pause is made true by either ESC or P being pressed
 	 */
 	public boolean pause;
-	
-	/**
-	 * @return The x coordinate of the mouse
-	 */
-	public static int getX() {
-		return mouseX;
-	}
-	
-	/**
-	 * @return The y coordinate of the mouse
-	 */
-	public static int getY() {
-		return mouseY;
-	}
-	
-	/**
-	 * @return Which mouse button is pressed:
-	 * <ul>
-	 *     <li>-1: No button pressed</li>
-	 *     <li>0: Left mouse button is pressed</li>
-	 *     <li>1: Right mouse button is pressed</li>
-	 *     <li>2: Middle mouse button is pressed</li>
-	 * </ul>
-	 */
-	public static int getButton() {
-		return mouseB;
-	}
 	
 	/**
 	 * Updates the state of the keys in keys array
@@ -89,27 +47,21 @@ public class OtherInput implements InputProcessor {
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		mouseB = button;
 		return false;
 	}
 
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		mouseB = -1;
 		return false;
 	}
 
 	@Override
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		mouseX = screenX;
-		mouseY = screenY;
 		return false;
 	}
 
 	@Override
 	public boolean mouseMoved(int screenX, int screenY) {
-		mouseX = screenX;
-		mouseY = screenY;
 		return false;
 	}
 
