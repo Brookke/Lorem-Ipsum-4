0ac16c32ae171be3d667fe41516dbe1f550e3020
Nick <naw.vaughan@gmail.com>
2017-02-13 16:45:15 +0000

    Removed keys array and pause variable

--- core/src/me/lihq/game/people/controller/GlobalInput.java
+++ core/src/me/lihq/game/people/controller/GlobalInput.java
@@ -10,34 +10,20 @@ import com.badlogic.gdx.InputAdapter;
  */
 public class GlobalInput extends InputAdapter {
 	
-	/**
-	 * Private array storing the boolean state of keys. The index of the array is the key value from libgdx. True means key is pressed, False means key is not pressed
-	 */
-	private boolean[] keys = new boolean[256];
-	
-	/**
-	 * Public variable(s) representing if the key(s) corresponding to certain actions are pressed
-	 * E.g. pause is made true by either ESC or P being pressed
-	 */
-	public boolean pause;
-	
-	/**
-	 * Updates the state of the keys in keys array
-	 */
-	public void update() {
-		pause = keys[Input.Keys.ESCAPE] || keys[Input.Keys.P];
-	}
-	
 	@Override
 	public boolean keyDown(int keycode) {
-		keys[keycode] = true;
 		return false;
 	}
 
 	@Override
 	public boolean keyUp(int keycode) {
-		keys[keycode] = false;
 		return false;
+	}
+	
+	/**
+	 * Called once a frame to transfer read input to the live game data in the logic Thread.
+	 */
+	public void update() {
 	}
 
 }
