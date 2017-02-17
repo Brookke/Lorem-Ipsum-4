a1a2c2e4454970d04b9eb64e7a7be4fe8be40f30
Nick <naw.vaughan@gmail.com>
2017-02-17 20:26:01 +0000

    Added WinScreen stub

---
+++ core/src/me/lihq/game/screen/WinScreen.java
@@ -0,0 +1,61 @@
+package me.lihq.game.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+import me.lihq.game.GameMain;
+
+public class WinScreen extends AbstractScreen {
+	
+	private Stage stage;
+
+	public WinScreen(GameMain game) {
+		super(game);
+
+        stage = new Stage(new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        
+        
+	}
+
+	@Override
+	public void show() {
+		// Add the stage to the input multiplexer, so it can receive input
+		// without blocking other input controllers
+        game.inputMultiplexer.addProcessor(stage);
+	}
+
+	@Override
+	public void update() {
+	}
+
+	@Override
+	public void render(float delta) {
+	}
+
+	@Override
+	public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void hide() {
+		// Remove the stage from the input multiplexer, so it doesn't fire
+		// event listeners when the screen is not visible
+        game.inputMultiplexer.removeProcessor(stage);
+	}
+
+	@Override
+	public void dispose() {
+		stage.dispose();
+	}
+
+}
