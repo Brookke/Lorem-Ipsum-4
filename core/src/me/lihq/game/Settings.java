5df2f2eff9f209e542ed6ef02d2d0db68e31474c
Nawv <naw.vaughan@gmail.com>
2017-02-20 15:08:36 +0000

    Attributed code to us

--- core/src/me/lihq/game/Settings.java
+++ core/src/me/lihq/game/Settings.java
@@ -24,35 +24,47 @@ public class Settings {
     
     /**
      * The size of the clue textures
+     *
+     * @author JAAPAN
      */
     public static final int CLUE_SIZE = 64;
     
     /**
      * The number of clues to be used in a given game
+     *
+     * @author JAAPAN
      */
     public static final int NUMBER_OF_CLUES = 10;
     
     /**
      * The number of clues which should be red herrings; i.e. point
      * the player in the wrong direction
+     *
+     * @author JAAPAN
      */
     public static final int NUMBER_OF_RED_HERRINGS = 4;
   
     /**
      * The muted state of the game. Initially false, changed via the checkbox
      * in SettingsScreen. When muted, the music is paused and sound effects do not play
+     *
+     * @author JAAPAN
      */
     public static boolean MUTED = false;
     
     /**
      * The volume of the music. Represented by a value between 0 (effectively muted) and
      * 1 (full volume)
+     *
+     * @author JAAPAN
      */
     public static float MUSIC_VOLUME = 0.5f;
     
     /**
      * The volume of the sound effects. Represented by a value between 0 (effectively muted) 
      * and 1 (full volume)
+     *
+     * @author JAAPAN
      */
     public static float SFX_VOLUME = 1f;
 
