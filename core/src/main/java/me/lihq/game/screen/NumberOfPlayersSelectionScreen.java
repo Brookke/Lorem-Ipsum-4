16e0644071159e96b3089894b86ae94be513243c
beno11224 <beno11224@gmail.com>
2017-03-05 12:06:26 +0000

    add new screen class to hold numberofplayers slider

---
+++ core/src/main/java/me/lihq/game/screen/NumberOfPlayersSelectionScreen.java
@@ -0,0 +1,46 @@
+package me.lihq.game.screen;
+
+import me.lihq.game.GameMain;
+
+/**
+ * Created by Beno11224 on 05/03/2017.
+ */
+public class NumberOfPlayersSelectionScreen extends AbstractScreen{
+
+    public NumberOfPlayersSelectionScreen(GameMain game)
+    {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void show() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+}
