8ca520fd75d0a8e76d80e8c899ee79b1628ab277
beno11224 <beno11224@gmail.com>
2017-03-06 18:49:53 +0000

    ScoreBoard works, need to make it look nice now.

--- core/src/main/java/me/lihq/game/screen/PlayerSwitchScreen.java
+++ core/src/main/java/me/lihq/game/screen/PlayerSwitchScreen.java
@@ -5,13 +5,13 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import me.lihq.game.Assets;
 import me.lihq.game.GameMain;
+import me.lihq.game.GameSnapshot;
 import me.lihq.game.screen.elements.Menu;
 import me.lihq.game.screen.elements.UIHelpers;
 
@@ -63,6 +63,12 @@ public class PlayerSwitchScreen extends AbstractScreen {
         text.setPosition(Gdx.graphics.getWidth() / 2 - (text.getWidth() / 2), Gdx.graphics.getHeight() * 3 / 4);
 
         /**
+         * Set up the label to tell which player to go next
+         */
+        next = UIHelpers.createLabel("Batman, are you ready?", Assets.TITLE_FONT, Color.WHITE);
+        next.setPosition(Gdx.graphics.getWidth() / 2 - (next.getWidth() / 2), Gdx.graphics.getHeight() / 2);
+
+        /**
          * Set up the button to click
          */
         TextButton continueButton;
@@ -71,10 +77,22 @@ public class PlayerSwitchScreen extends AbstractScreen {
         continueButton.setPosition((Gdx.graphics.getWidth() / 2) - (Menu.BUTTON_WIDTH / 2), Gdx.graphics.getHeight() / 2 + Menu.BUTTON_HEIGHT + 20);
 
         /**
-         * Set up the label to tell which player to go next
+         * Set up the current LeaderBoard //could add a background?
          */
-        next = UIHelpers.createLabel("Batman, are you ready?", Assets.TITLE_FONT, Color.WHITE);
-        next.setPosition(Gdx.graphics.getWidth() / 2 - (next.getWidth() / 2), Gdx.graphics.getHeight() / 2);
+        List<String> scoreBoard = new List<String>(Assets.UI_SKIN);
+        //get each player + the players score
+        Array<String> arrayPlayers = new Array<String>(game.noPlayers+1);
+        int count = 0;
+        arrayPlayers.add("");//blank one for a gap so the selected player doesn't go weird
+        for (GameSnapshot snapshot: game.gameSnapshots)
+        {
+            arrayPlayers.add(snapshot.player.getName()+" Score: "+snapshot.player.getScore());
+            count++;
+        }
+        scoreBoard.setItems(arrayPlayers);
+        //set location of scoreBoard
+        scoreBoard.setPosition((Gdx.graphics.getWidth() / 2), Gdx.graphics.getHeight() / 2 - Gdx.graphics.getHeight()/8);
+
 
         /**
          * This is the background so that the UI is visible
@@ -82,14 +100,15 @@ public class PlayerSwitchScreen extends AbstractScreen {
         background = new Image(UIHelpers.createBackgroundTexture(new Color(0, 0, 0, 0.7f), (int) (next.getWidth() + 40), (int) ((text.getY() + text.getHeight()) - next.getY())));
         background.setPosition(Gdx.graphics.getWidth() / 2 - (background.getWidth() / 2), Gdx.graphics.getHeight() / 2);
 
-        stage.addActor(background);
 
         /**
          * Add the actors and a button listener
          */
+        stage.addActor(background);
         stage.addActor(next);
         stage.addActor(text);
         stage.addActor(continueButton);
+        stage.addActor(scoreBoard);
         continueButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
