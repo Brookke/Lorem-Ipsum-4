3045660612f67db74e323cf344800b1ea37c02f8
Brooke Hatton <hatton.brooke@gmail.com>
2017-04-24 22:13:41 +0100

    remove redundant call

--- core/src/main/java/me/lihq/game/screen/PuzzleScreen.java
+++ core/src/main/java/me/lihq/game/screen/PuzzleScreen.java
@@ -29,7 +29,7 @@ public class PuzzleScreen extends AbstractScreen {
 
         puzzle.init();
 
-        if (game.currentSnapshot.puzzle.isSolved()) {
+        if (puzzle.isSolved()) {
             puzzle.goToSecretRoom();
         }
 
