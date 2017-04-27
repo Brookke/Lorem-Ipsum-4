5f5224c33fb89889d708fa0aa808b6317c40ce84
Joe Shuff <joe.shuff@btinternet.com>
2017-04-27 21:54:22 +0100

    Document Map.java

--- core/src/main/java/me/lihq/game/models/Map.java
+++ core/src/main/java/me/lihq/game/models/Map.java
@@ -184,7 +184,11 @@ public class Map {
         return null;
     }
 
-
+    /**
+     * This method returns the Room Transition that takes the player from the Secret Room to the Main Room
+     * 
+     * @return (Room.Transition) the room transition data
+     */
     public Room.Transition getSecretRoomTransition() {
         return secretRoomTrans;
     }
